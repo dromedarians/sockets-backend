878b1f2b077d8e8a2336f81ddbb1725e7ff38589
Michael Flis <michaelflis07@gmail.com>
2019-01-31 20:34:47 -0800

    begins build for backend server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+app.listen(8080, () => {
+  console.log('Server connected on port 8080')
+})
