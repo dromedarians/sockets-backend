52018021809986e68a42ab50c9f81a123a25b1f5
vasyl-n <vasyl.nrk@gmail.com>
2019-02-25 21:19:12 -0800

    adds logs

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  console.log(socket.server.clients)
   console.log('a user connected');
   
   socket.on('disconnect', function(){
